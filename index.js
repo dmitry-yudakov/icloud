20111ad81d28a3a68d8d8f53a50278babc6714f8
Thomas Hessler <thessler@Thomass-MacBook-Pro.local>
2015-08-01 21:38:45 -0400

    trying to get this guid added

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ module.exports = function() {
             locale : "en_US",
             usertz : "America/Los_Angeles",
             lang   : "en",
-            pguid   : event.pguid
+            calendar_guid : event.guid
         });
         var url = session.webservices.calendar.url.replace(':443', '');
         req.post({
