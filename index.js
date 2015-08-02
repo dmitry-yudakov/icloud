b3932076693871f895e544d041ce022c2f3acd60
Thomas Hessler <thessler@Thomass-MacBook-Pro.local>
2015-08-02 14:47:05 -0400

    setting calendar to default

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ module.exports = function() {
                 "endDate"  : event.endDate,
                 "title"    : event.title,
                 "location" : event.location,
-                "calendar" : 'work'
+                "calendar" : 'default'
             },
             headers : {
                 host : session.webservices.calendar.url.split('//')[1].split(':')[0],
