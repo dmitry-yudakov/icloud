b2096d148bc51f10a5d6f0f105f3c4d8399d937b
Thomas Hessler <thessler@Thomass-MacBook-Pro.local>
2015-08-02 14:39:02 -0400

    change guid

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ module.exports = function() {
                 "endDate"  : event.endDate,
                 "title"    : event.title,
                 "location" : event.location,
-                "calendar" : session.webservices.calendar.guid
+                "calendar" : 'work'
             },
             headers : {
                 host : session.webservices.calendar.url.split('//')[1].split(':')[0],
